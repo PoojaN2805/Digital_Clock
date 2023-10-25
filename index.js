142f1f4c42c04575b0698f197d6d7da8cf14b2a2
Poojanagaraj <85775311+PoojaN2805@users.noreply.github.com>
2023-10-25 10:33:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,13 +14,24 @@ function updateClock() {
     }, 1000);
 }
 
-function updateDate(){
-    let day=document.getElementById("day");
-    let date= document.getElementById("date");
-    let month=document.getElementById("month");
+function updateDateTime() {
+    const now = new Date();
+
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    const day = daysOfWeek[now.getDay()];
+    const date = now.getDate();
+    const month = months[now.getMonth()];
+
+    document.getElementById("day").textContent = `${day}`;
+    document.getElementById("date").textContent = `${date}`;
+    document.getElementById("month").textContent = `${month}`;
 }
+
+updateDateTime();
+setInterval(updateDateTime, 1000);
 
 window.onload = function () {
     updateClock();
-    updateDate();
 };
